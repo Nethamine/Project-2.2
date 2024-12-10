266cb6308922530039ec0c5e68e0fcf47bb3af20
Nethamine <nathan.phang123@gmail.com>
2024-12-10 15:30:16 +0100

    Update Curve_fit.py
    code van mike, file kan veranderd worden

--- Curve_fit.py
+++ Curve_fit.py
@@ -1,25 +1,32 @@
-from scipy.optimize import curve_fit
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.optimize import curve_fit
 
-d = 1e-3
-l = 532e-9
+file: str = "/Users/nathanphang/metingen.csv" 
 
-generations = 5
+df = pd.read_csv(file, names=["hoek", "N1", "N2", "N3"])
+ 
+df["Ngem"] = (df["N1"] + df["N2"] + df["N3"])/3
+df["stdv"] = (((df["N1"] - df["Ngem"])**2 + (df["N2"] - df["Ngem"])**2 + (df["N3"] - df["Ngem"])**2)/ 2)**(1/2)
 
 
-def model_N(deg, n):
-    N = (d / l) * (n / n-1) * deg**2    
-    return N
+Nlijst = df["Ngem"]
 
-a = np.linspace(0,10,20) # DEGREES
-n = # DATA
+df["radialen"] = df["hoek"] * (2*np.pi)/360
 
-sigma = 0
+radialenlijst = df["radialen"].to_numpy()
 
-for i in range(5)
-    popt, pcov = curve_fit(model_N, a, n,p0=[[0,2],[0,5]], sigma=sigma)
-                           
-    # bereken fout
-    # sigma = abs(N_praktish - N_theoretisch)
-    
-print(popt,pcov)
+def model(x, n):
+    n_lucht = 1.0029
+    d: float = 2e-3
+    lambda: float = 532e-9
+
+    theta = np.arcsin(np.sin(x)*n_lucht/n)
+
+
+    N = 2*d/lambda*(n/np.cos(theta) + np.tan(x)*np.sin(x)*n_lucht - np.tan(theta)*np.sin(x)*n_lucht - (n-n_lucht) -n_lucht/np.cos(x))
+
+return N 
+
+popt, pcov = curve_fit(model, df["radialen"], df["Ngem"], pO=[1.5], sigma=df["stdv"], absolute_sigma=True)
